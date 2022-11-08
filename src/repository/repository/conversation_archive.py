d7702f49bb97b29fc1df9f3a07182aac4c2b8668
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-11-08 05:57:37 +0100

    integration tests for archive and refactoring

--- src/repository/repository/conversation_archive.py
+++ src/repository/repository/conversation_archive.py
@@ -14,7 +14,7 @@ Institute for Computational Linguistics
 
 """
 from typing import TypedDict
-
+from enum import Enum
 import sys
 import csv
 import src.helpers.helpers.helpers as helpers
@@ -23,25 +23,47 @@ class ArchiveRecord(TypedDict):
     answer: str
     question: str
 
+class ArchiveOperation(Enum):
+    READ = 'r',
+    WRITE = 'w'
+
 class ConversationArchival:
     def __init__(self, creation_date):
         self.archive_dir = helpers.get_project_path() + '/src/repository/data/conversation_archive/'
-        self.patient_file = creation_date + '.csv'
+        self.archive_file_name = creation_date + '.csv'
         self.fieldnames = ['answer', 'question']
         self.is_ready = False
         self.archive_file = None
         self.archive_writer = None
-        self.start_archive()
+        self.start(ArchiveOperation.WRITE)
 
-    def start_archive(self):
-        helpers.create_directory_if_not_exists(self.archive_dir)
-        self.archive_file = open(self.archive_dir + self.patient_file, 'w')
-        self.archive_writer = csv.DictWriter(self.archive_file, delimiter='\t', quotechar='"', fieldnames=self.fieldnames)
-        self.archive_writer.writeheader()
-        self.is_ready = True
+    def start(self, operation: ArchiveOperation):
+        if self.is_ready:
+            print("Archive is already running.")
+        else:
+            helpers.create_directory_if_not_exists(self.archive_dir)
+            if operation is ArchiveOperation.WRITE:
+                self.archive_file = open(self.archive_dir + self.archive_file_name, 'w')
+                self.archive_writer = csv.DictWriter(self.archive_file,
+                                                     delimiter='\t',
+                                                     quotechar='"',
+                                                     fieldnames=self.fieldnames)
+                self.archive_writer.writeheader()
+            elif operation is ArchiveOperation.READ:
+                self.archive_file = open(self.archive_dir + self.archive_file_name, 'r')
+            else:
+                sys.exit("Unknown archive operation. Allowed are: READ, WRITE")
+            self.is_ready = True
 
-    def write_archive_record(self, archive_record: ArchiveRecord):
+    def write(self, archive_record: ArchiveRecord):
         if self.is_ready:
             self.archive_writer.writerow(archive_record)
         else:
             sys.exit("Start archive before writing to archive by using the start_archive() method.")
+
+    def terminate(self):
+        if self.is_ready:
+            self.archive_file.close()
+            self.is_ready = False
+        else:
+            print("No need for terminating, the archive is not running.")
--- tests/integration/archive_test.py
+++ tests/integration/archive_test.py
@@ -1,13 +1,28 @@
 import unittest
-from src.repository.repository.conversation_archive import ConversationArchival
+import csv
+from src.repository.repository.conversation_archive import ConversationArchival, ArchiveOperation
 
 class ConversationArchiveTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.archive = ConversationArchival('creation_date')
+        self.archive = ConversationArchival('tests_creation_date')
 
     def test_archive_is_ready_after_instantiation(self):
         self.assertTrue(self.archive.is_ready)
         self.assertIsNotNone(self.archive.archive_writer)
         self.assertIsNotNone(self.archive.archive_file)
 
+    def test_write_conversation_record_to_archive_file(self):
+        archive_record = {'answer': 'I have headache', 'question': 'When do you have headache?'}
+        self.archive.write(archive_record)
+        self.archive.terminate()
+        self.archive.start(ArchiveOperation.READ)
+        reader = csv.reader(self.archive.archive_file, delimiter='\t', quotechar='"')
+        header = next(reader)
+        first_row = next(reader) #
+        self.assertEqual(['answer', 'question'], header)
+        self.assertEqual(['I have headache', 'When do you have headache?'], first_row)
+
+    def test_terminate_archive(self):
+        self.archive.terminate()
+        self.assertFalse(self.archive.is_ready)
